11f62708f97c4b25094220605ef98c6a34234262
Piruin Panichphol <piruin.p@gmail.com>
2019-07-20 16:39:14 +0700

    Workshop - Get user input

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.helloworld
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.fab
+import kotlinx.android.synthetic.main.activity_main.toolbar
+import kotlinx.android.synthetic.main.content_login.login
+import kotlinx.android.synthetic.main.content_login.password
+import kotlinx.android.synthetic.main.content_login.username
 
 class MainActivity : AppCompatActivity() {
+
+    private val userPassMap = mapOf(
+            "admin" to "@dm1n555",
+            "user1" to "helloworld@tu",
+            "android" to "12341234"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +28,20 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        login.setOnClickListener {
+            val inputUser = username.text.toString()
+            val inputPass = password.text.toString()
+
+            val pass = userPassMap[inputUser]
+            if (pass == inputPass) {
+                Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show()
+                username.text = null
+                password.text = null
+            } else {
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
